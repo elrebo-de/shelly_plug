0e0ef1bcd7c33b819d3fb15b1ff044ad2dda0c81
Christoph.Oberle.ext <christoph.oberle.ext@shbversicherung.de>
2026-02-05 22:01:31 +0100

    version 1.0.3 - example improved

--- examples/shelly_plug_example/main/main.cpp
+++ examples/shelly_plug_example/main/main.cpp
@@ -3,68 +3,41 @@
  */
 
 #include <string>
-#include "shelly_plug.hpp"
 #include "esp_log.h"
-
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "soc/clk_tree_defs.h"
 
-#include "nvs_flash.h"
-#include "ssid_manager.h"
-#include "wifi_station.h"
-#include "wifi_configuration_ap.h"
+//#include "soc/clk_tree_defs.h"
+
+#include "wifi_manager.hpp"
+#include "shelly_plug.hpp"
 
 static const char *tag = "ShellyPlug";
 
 extern "C" void app_main(void)
 {
-      // Initialize the default event loop
-      ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-      // Initialize NVS flash for Wi-Fi configuration
-      esp_err_t ret = nvs_flash_init();
-      if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-          ESP_ERROR_CHECK(nvs_flash_erase());
-          ret = nvs_flash_init();
-      }
-      ESP_ERROR_CHECK(ret);
-
-      // Get the Wi-Fi configuration
-      auto& ssid_list = SsidManager::GetInstance().GetSsidList();
-      if (ssid_list.empty()) {
-          // Start the Wi-Fi configuration AP
-          auto& ap = WifiConfigurationAp::GetInstance();
-          ap.SetSsidPrefix("ESP32");
-          ap.SetLanguage("de-DE");
-          ap.Start();
-          return;
-      }
-
-      // Otherwise, connect to the Wi-Fi network
-      WifiStation::GetInstance().Start();
-
-      vTaskDelay(30000 / portTICK_PERIOD_MS);
-
+    Wifi wifi( std::string("WifiManager"), // tag for ESP_LOGx
+               std::string("ESP32"),       // ssid_prefix for configuration access point
+               std::string("de-DE")        // language for configuration access point
+             );
 
     /* Configure the ShellyPlug */
-    {
-        std::string ipAddrShellyPlug = std::string("192.168.178.102");
-        ShellyPlug shellyWozi(std::string("shellyWozi"), ipAddrShellyPlug);
+    std::string ipAddrShellyPlug = std::string("192.168.178.102");
 
-        if (shellyWozi.GetLastHttpCode() != HTTP_CODE_OK) {
-            ESP_LOGI(tag, "No Shelly Plug at IPAddr %s", ipAddrShellyPlug.c_str());
-        }
-        else {
-            shellyWozi.Switch(true, (uint16_t) 5);
-            vTaskDelay(10000 / portTICK_PERIOD_MS);
-            shellyWozi.Switch(true);
-            vTaskDelay(5000 / portTICK_PERIOD_MS);
-            shellyWozi.Switch(false);
-            vTaskDelay(5000 / portTICK_PERIOD_MS);
-            shellyWozi.Toggle();
-            vTaskDelay(5000 / portTICK_PERIOD_MS);
-            shellyWozi.Toggle();
-        }
+    ShellyPlug shellyWozi(std::string("shellyWozi"), ipAddrShellyPlug);
+
+    if (shellyWozi.GetLastHttpCode() != HTTP_CODE_OK) {
+        ESP_LOGI(tag, "No Shelly Plug at IPAddr %s", ipAddrShellyPlug.c_str());
+    }
+    else {
+        shellyWozi.Switch(true, (uint16_t) 5);
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
+        shellyWozi.Switch(true);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        shellyWozi.Switch(false);
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        shellyWozi.Toggle();
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+        shellyWozi.Toggle();
     }
 }
